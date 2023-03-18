e098d79085030f6d296afdf74c40b8d1191e5107
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-03-19 01:02:57 +0900

    トップページのスタイリングを調整 (#9)

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google"
 import Layout from "@/components/Layout"
 import utilStyles from "@/styles/utils.module.css"
 import Link from "next/link"
+import styles from "@/styles/Home.module.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,18 +14,68 @@ export default function Home() {
           私はフルスタックエンジニアです/Udemy講師として活動しています。好きな言語はJavaScriptです。
         </p>
       </section>
-      <section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2>🗒エンジニアのブログ</h2>
-        <div>
+        <div className={styles.grid}>
           <article>
             <Link href="/">
-              <img src="/images/thumbnail01.jpg" />
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
             </Link>
             <Link legacyBehavior href="/">
-              <a>SSGとSSRの使い分けの場面はいつなのか？</a>
+              <a className={utilStyles.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
             </Link>
             <br />
-            <small>February 23, 2020</small>
+            <small className={utilStyles.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail02.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link legacyBehavior href="/">
+              <a className={utilStyles.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail03.jpeg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link legacyBehavior href="/">
+              <a className={utilStyles.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail04.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link legacyBehavior href="/">
+              <a className={utilStyles.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23, 2020</small>
           </article>
         </div>
       </section>
