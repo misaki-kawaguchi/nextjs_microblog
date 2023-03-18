2212bc503fb2e54b1412b21c283f17cdd833972a
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-03-19 00:50:56 +0900

    トップページのレイアウトを更新 (#8)

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,33 @@
 import { Inter } from "next/font/google"
 import Layout from "@/components/Layout"
+import utilStyles from "@/styles/utils.module.css"
+import Link from "next/link"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>
+          私はフルスタックエンジニアです/Udemy講師として活動しています。好きな言語はJavaScriptです。
+        </p>
+      </section>
+      <section>
+        <h2>🗒エンジニアのブログ</h2>
+        <div>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" />
+            </Link>
+            <Link legacyBehavior href="/">
+              <a>SSGとSSRの使い分けの場面はいつなのか？</a>
+            </Link>
+            <br />
+            <small>February 23, 2020</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
