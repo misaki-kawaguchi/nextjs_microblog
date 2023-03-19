08bd82c16fd55caabe6145b4352f6d70506e7c5a
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-03-19 19:08:44 +0900

    getStaticPathsを追加する

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,13 @@
 import Layout from "@/components/Layout"
+import { getAllPostIds } from "@/lib/post"
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
 export default function Post() {
   return <Layout>動的ルーティング設定</Layout>
